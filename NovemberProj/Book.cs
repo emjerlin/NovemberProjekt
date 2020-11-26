8f68aaa699e6d9eb08751f0f4e6da76f7e8ea83a
emjerlin <60339583+emjerlin@users.noreply.github.com>
2020-11-26 16:29:30 +0100

    Name randomizer, book create

---
+++ NovemberProj/Book.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+namespace NovemberProjekt
+{
+    public class Book
+    {
+        public int price;
+        int actualValue;
+        string name;
+        int rarity;
+        string category;
+        bool cursed;
+        Random generator = new Random();
+        List<string> nameList = new List<string>(){"Ex Altiora", "The Boneturner's Tale"};
+        public Book(){
+
+            actualValue = generator.Next(1,101);
+            rarity = generator.Next(1,11);
+
+            int b = generator.Next(2);
+            if (b == 0){
+                cursed = true;
+            }
+            else{
+                cursed = false;
+            }
+            int length = nameList.Count;
+            int y = length--;
+            int x = generator.Next(0,y);
+            name = nameList[x];
+        }
+
+
+        public void PrintInfo(){
+            Console.WriteLine(name);
+            Console.WriteLine("Rarity level: " + rarity);
+            Console.WriteLine("Category: " + category);
+            Console.WriteLine("Price: " + price);
+        }
+    }
+}
--- NovemberProj/Program.cs
+++ NovemberProj/Program.cs
@@ -1,13 +1,24 @@
 ﻿using System;
 
-namespace NovemberProj
+namespace NovemberProjekt
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to your first day of many at the antiquary");
+            //Planen är att man ska kunna gå från rum till rum i en affär, och kunna plocka upp böcker som man kan köpa.
+            //Sedan andra delen av spelet kommer man tillbaka till sin egen affär och ska sälja den till customers
+            //Man får en rapport hur mycket man tjänar i slutet av dagen. Om man har kvar ett cursed item
+            //får man problem under nästa dag. Om man säljer en cursed item till en customer som inte letar efter det
+            //kommer man få en lawsuit, som man måste kunna betala av
+            Console.WriteLine();
+
+
+            Book b1 = new Book();
+            b1.PrintInfo();
             
+            Console.ReadLine();
         }
     }
 }
