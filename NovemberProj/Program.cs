1ed33738892fd3b6d072f16a84fb071cd1062d3f
emjerlin <60339583+emjerlin@users.noreply.github.com>
2020-12-08 13:28:23 +0100

    Raylib more...

--- NovemberProj/Program.cs
+++ NovemberProj/Program.cs
@@ -15,29 +15,43 @@ namespace NovemberProjekt
 
                 Raylib.BeginDrawing();
 
-                Raylib.ClearBackground(Color.GRAY);
+                Raylib.ClearBackground(Color.WHITE);
 
                 Raylib.DrawText("Welcome to your first day of many at the antiquary", 60, 80, 30, Color.BLACK);
 
-                Rectangle roadHorizontal = new Rectangle(60,600,400, 60);
-                Rectangle roadVertical = new Rectangle(60,200,60, 400);
+                Rectangle background1 = new Rectangle (0,0, 320,660);
+                Rectangle background2 = new Rectangle (0,660,540,340);
+                Rectangle background3 = new Rectangle (380,0, 620,600);
+                Rectangle roadHorizontal = new Rectangle(320,600,480, 60);
+                Rectangle roadVertical = new Rectangle(320,200,60,400);
                 Rectangle player = new Rectangle(posX,posY,20,20);
-            
-                Raylib.DrawRectangle(200,200,300,120,Color.RED);
 
+                Raylib.DrawRectangleRec(background1,Color.BLUE);
+                Raylib.DrawRectangleRec(background2,Color.PINK);
+                Raylib.DrawRectangleRec(background3,Color.GREEN);
 
                 Raylib.DrawRectangleRec(roadVertical,Color.DARKGRAY);
-
                 Raylib.DrawRectangleRec(roadHorizontal,Color.DARKGRAY);
+            
+                Raylib.DrawRectangle(200,200,300,120,Color.RED);
+                Raylib.DrawRectangle(325,270,60,50,Color.BLACK);
+                Raylib.DrawText("Second Hand Store", 215, 230, 25, Color.BLACK);
+
+                Raylib.DrawRectangle(500,400,400,200,Color.PURPLE);
+                Raylib.DrawRectangle(670,550,60,50,Color.BLACK);
+                Raylib.DrawText("ANTIQUARY", 590, 450, 35, Color.BLACK);
+
+               
+                
+               
 
                 Raylib.DrawRectangleRec(player,Color.PURPLE);
 
-                bool isCollidingH = Raylib.CheckCollisionRecs(roadHorizontal, player);
+                bool isCollidingBG = Raylib.CheckCollisionRecs(background1, player);
                 bool isCollidingV = Raylib.CheckCollisionRecs(roadVertical, player);
                 
 
-                if (isCollidingV == true)
-                {
+                
                 if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT))
                 {
                    posX--;
@@ -54,15 +68,17 @@ namespace NovemberProjekt
                 {
                     posY--;
                 }
-                }
-                else{
-
+                
+                if (isCollidingBG == true){
+                    posX++;
                 }
                 
-
-                Book b1 = new Book();
-
-                b1.PrintInfo();
+                if (Raylib.IsKeyPressed(KeyboardKey.KEY_SPACE)){
+                     Book b1 = new Book();
+                     
+                     b1.PrintInfo();
+                }
+               
                 
 
                 Raylib.EndDrawing();
